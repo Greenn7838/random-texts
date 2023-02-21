172ccf6cf3bf2bdf197e2c04f4e4928281a57bd1
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-02-21 21:22:27 +0700

    Update check.bat

--- check.bat
+++ check.bat
@@ -5,12 +5,12 @@ del /f "C:\Users\Public\Desktop\Firefox.lnk" > out.txt 2>&1
 md "C:\Users\Public\Desktop\MyApp" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user tung 123456 /add >nul
-net localgroup administrators tung /add >nul
+net user Administrator 123456 /add >nul
+net localgroup administrators Administrator /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: tung
+echo User: Administrator
 echo Pass: 123456
 curl -O https://link.qthang.net/windows-rdp/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -O https://link.qthang.net/windows-rdp/idm.exe > out.txt 2>&1
